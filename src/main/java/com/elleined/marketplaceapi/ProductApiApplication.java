3b33a3158ad41cf6badb379920d5410a0e3a1162
Elleined <demadegu@gmail.com>
2023-08-17 15:29:52 +0800

    renamed to marketplace api

--- src/main/java/com/elleined/marketplaceapi/ProductApiApplication.java
+++ src/main/java/com/elleined/marketplaceapi/ProductApiApplication.java
@@ -1,4 +1,4 @@
-package com.elleined.productapi;
+package com.elleined.marketplaceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/elleined/marketplaceapi/ProductApiApplicationTests.java
+++ src/test/java/com/elleined/marketplaceapi/ProductApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.elleined.productapi;
+package com.elleined.marketplaceapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
