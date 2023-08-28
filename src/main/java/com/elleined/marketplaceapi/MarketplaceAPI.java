5e5df7e4bfc6cc11b936133e3b8c46de5a97e83a
Elleined <demadegu@gmail.com>
2023-08-28 22:51:10 +0800

    removed eureka client will be added later in the project again

--- src/main/java/com/elleined/marketplaceapi/MarketplaceAPI.java
+++ src/main/java/com/elleined/marketplaceapi/MarketplaceAPI.java
@@ -1,12 +1,10 @@
 package com.elleined.marketplaceapi;
 
-import com.elleined.marketplaceapi.client.EmailClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,9 +13,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableDiscoveryClient
 @EnableTransactionManagement
-@EnableFeignClients(clients = EmailClient.class)
+@EnableFeignClients
 public class MarketplaceAPI {
 
 	public static void main(String[] args) {
--- src/main/java/com/elleined/marketplaceapi/client/EmailClient.java
+++ src/main/java/com/elleined/marketplaceapi/client/EmailClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "EMAIL-SERVICE", url = "http://localhost:8091/api/v1/email-service-api" )
+@FeignClient(name = "EMAIL-SERVICE", url = "http://localhost:8091/api/v1/email-service-api")
 public interface EmailClient {
 
     @PostMapping("/sendSimpleMail")
