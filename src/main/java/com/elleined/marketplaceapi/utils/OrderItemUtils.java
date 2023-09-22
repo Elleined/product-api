5d9d8680bbc77bc40de4d766a3b2c5cf6fcf1e2b
Elleined <demadegu@gmail.com>
2023-09-23 01:09:06 +0800

    date range fix

--- src/main/java/com/elleined/marketplaceapi/utils/OrderItemUtils.java
+++ src/main/java/com/elleined/marketplaceapi/utils/OrderItemUtils.java
@@ -9,7 +9,8 @@ public interface OrderItemUtils {
     static List<OrderItem> getOrderItemsByRange(List<OrderItem> orderItems, LocalDate start, LocalDate end) {
         return orderItems.stream()
                 .filter(orderItem -> orderItem.getUpdatedAt().toLocalDate().isEqual(start)
-                        || (orderItem.getUpdatedAt().toLocalDate().isAfter(start) && orderItem.getUpdatedAt().toLocalDate().isBefore(end)))
+                        || (orderItem.getUpdatedAt().toLocalDate().isAfter(start) && orderItem.getUpdatedAt().toLocalDate().isBefore(end))
+                        || orderItem.getUpdatedAt().toLocalDate().equals(end))
                 .toList();
     }
 }
