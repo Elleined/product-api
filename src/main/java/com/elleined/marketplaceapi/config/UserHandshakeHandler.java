d572d9aed4cbbec8dbeeb8c5f1b2c58498f006f2
Elleined <demadegu@gmail.com>
2023-09-23 09:37:57 +0800

    revert ws config refactored

--- src/main/java/com/elleined/marketplaceapi/config/UserHandshakeHandler.java
+++ src/main/java/com/elleined/marketplaceapi/config/UserHandshakeHandler.java
@@ -13,16 +13,19 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import java.security.Principal;
 import java.util.Map;
-import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class UserHandshakeHandler extends DefaultHandshakeHandler {
+
+    @Autowired
+    private HttpSession session;
     @Override
      protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
-        final String username = UUID.randomUUID().toString();
-        log.debug("User with generated username of {} connected to private chat", username);
-        return new StompPrincipal(username);
+        
+        User currentUser = (User) session.getAttribute("currentUser");
+        log.debug("User with id of {} connected to the private chat", currentUser.getId());
+        return new UserPrincipal(String.valueOf(currentUser.getId()));
     }
 }
