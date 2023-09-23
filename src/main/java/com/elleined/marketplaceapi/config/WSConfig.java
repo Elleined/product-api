dbc68aec0fcb797422e447b71ed1f62d802a4dd4
Elleined <demadegu@gmail.com>
2023-09-23 13:06:33 +0800

    ws config refactored

--- src/main/java/com/elleined/marketplaceapi/config/WSConfig.java
+++ src/main/java/com/elleined/marketplaceapi/config/WSConfig.java
@@ -21,7 +21,7 @@ public class WSConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/private-chat", "/public-chat", "/notification");
+        registry.enableSimpleBroker("/private", "/public", "/notification");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
