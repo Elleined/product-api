06fd38dd22d4374ad10320c97efa8a850cd1501f
Denielle Mar M. De Guzman <demadegu@gmail.com>
2023-10-05 19:50:47 +0800

    changed picture directories upload remove spaces

--- src/main/java/com/elleined/marketplaceapi/utils/DirectoryPaths.java
+++ src/main/java/com/elleined/marketplaceapi/utils/DirectoryPaths.java
@@ -1,10 +1,10 @@
 package com.elleined.marketplaceapi.utils;
 
 public interface DirectoryPaths {
-    String PROFILE_PICTURES_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\Profile Pictures\\";
-    String VALID_IDS_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\Valid Ids\\";
-    String SHOP_PICTURES_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\Shop Pictures\\";
-    String PRODUCT_PICTURES_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\Product Pictures\\";
-    String DEPOSIT_TRANSACTIONS_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\Deposit Transactions Pictures\\";
-    String WITHDRAW_TRANSACTIONS_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\Withdraw Transaction Pictures\\";
+    String PROFILE_PICTURES_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\ProfilePictures\\";
+    String VALID_IDS_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\ValidIds\\";
+    String SHOP_PICTURES_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\ShopPictures\\";
+    String PRODUCT_PICTURES_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\ProductPictures\\";
+    String DEPOSIT_TRANSACTIONS_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\DepositTransactionsPictures\\";
+    String WITHDRAW_TRANSACTIONS_PATH = "C:\\Users\\Acer\\Desktop\\croptrade projects\\croptrade-marketplace\\src\\main\\resources\\static\\img\\WithdrawTransactionPictures\\";
 }
