dc5f41d694c808092f8f08e762af10dba19c2535
Elleined <demadegu@gmail.com>
2023-10-22 09:26:24 +0800

    added retail and wholesale unit concrete class with annotations

---
+++ src/main/java/com/elleined/marketplaceapi/service/unit/RetailUnitService.java
@@ -0,0 +1,33 @@
+package com.elleined.marketplaceapi.service.unit;
+
+import com.elleined.marketplaceapi.exception.resource.ResourceNotFoundException;
+import com.elleined.marketplaceapi.model.unit.Unit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+@Service
+@Primary
+public class RetailUnitService implements UnitService {
+    @Override
+    public Unit save(String name) {
+        return null;
+    }
+
+    @Override
+    public Unit getById(int id) throws ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<String> getAll() {
+        return null;
+    }
+}
