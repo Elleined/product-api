26f315fb4c3dfe145a8c5e1eb2f71e3159de5aa2
Elleined <demadegu@gmail.com>
2023-10-24 22:10:57 +0800

    added retail order mapper

--- src/main/java/com/elleined/marketplaceapi/mapper/order/RetailOrderMapper.java
+++ src/main/java/com/elleined/marketplaceapi/mapper/order/RetailOrderMapper.java
@@ -2,11 +2,10 @@ package com.elleined.marketplaceapi.mapper.order;
 
 import com.elleined.marketplaceapi.dto.order.OrderDTO;
 import com.elleined.marketplaceapi.model.order.Order;
-import com.elleined.marketplaceapi.model.order.WholeSaleOrder;
+import com.elleined.marketplaceapi.model.order.RetailOrder;
 import com.elleined.marketplaceapi.model.user.User;
 import com.elleined.marketplaceapi.service.address.AddressService;
-import com.elleined.marketplaceapi.service.product.wholesale.WholeSaleProductService;
-import org.mapstruct.Context;
+import com.elleined.marketplaceapi.service.product.retail.RetailProductService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -14,37 +13,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 
 @Mapper(componentModel = "spring", imports = Order.OrderStatus.class)
-public abstract class WholeSaleOrderMapper implements OrderMapper<OrderDTO, WholeSaleOrder> {
+public abstract class RetailOrderMapper implements OrderMapper<OrderDTO, RetailOrder> {
 
     @Autowired
     @Lazy
-    private WholeSaleProductService wholeSaleProductService;
-
+    private RetailProductService retailProductService;
     @Autowired
     @Lazy
     private AddressService addressService;
-
     @Override
     @Mappings({
             @Mapping(target = "id", ignore = true),
             @Mapping(target = "sellerMessage", ignore = true),
+            @Mapping(target = "price", ignore = true),
 
-            @Mapping(target = "price", source = "price"),
             @Mapping(target = "orderDate", expression = "java(java.time.LocalDateTime.now())"),
             @Mapping(target = "orderStatus", expression = "java(OrderStatus.PENDING)"),
-            @Mapping(target = "deliveryAddress", expression = "java(addressService.getDeliveryAddressById(buyer, orderDTO.getDeliveryAddressId()))"),
-            @Mapping(target = "wholeSaleProduct", expression = "java(wholeSaleProductService.getById(orderDTO.getProductId()))"),
+            @Mapping(target = "deliveryAddress", expression = "java(addressService.getDeliveryAddressById(buyer, orderItemDTO.getDeliveryAddressId()))"),
+            @Mapping(target = "retailProduct", expression = "java(retailProductService.getById(orderItemDTO.getProductId()))"),
             @Mapping(target = "purchaser", expression = "java(buyer)"),
             @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())")
     })
-    public abstract WholeSaleOrder toEntity(OrderDTO orderDTO, @Context User buyer);
+    public abstract RetailOrder toEntity(OrderDTO dto, User buyer);
 
     @Override
     @Mappings({
             @Mapping(target = "deliveryAddressId", source = "deliveryAddress.id"),
-            @Mapping(target = "productId", source = "wholeSaleProduct.id"),
+            @Mapping(target = "productId", source = "retailProduct.id"),
             @Mapping(target = "purchaserId", source = "purchaser.id"),
-            @Mapping(target = "sellerId", source = "wholeSaleProduct.seller.id")
+            @Mapping(target = "sellerId", source = "retailProduct.seller.id")
     })
-    public abstract OrderDTO toDTO(WholeSaleOrder wholeSaleOrder);
+    public abstract OrderDTO toDTO(RetailOrder retailOrder);
 }
