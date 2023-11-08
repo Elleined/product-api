b1e43028751bd4a73e4fa7d24beea24800a05f23
Elleined <demadegu@gmail.com>
2023-11-08 21:31:41 +0800

    resolve order in productservice

--- src/main/java/com/elleined/marketplaceapi/service/product/ProductService.java
+++ src/main/java/com/elleined/marketplaceapi/service/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.elleined.marketplaceapi.service.product;
 
 import com.elleined.marketplaceapi.exception.resource.ResourceNotFoundException;
+import com.elleined.marketplaceapi.model.order.Order;
 import com.elleined.marketplaceapi.model.product.Product;
 import com.elleined.marketplaceapi.model.user.User;
 
@@ -20,21 +21,21 @@ public interface ProductService<T extends Product> {
 
     List<T> searchProductByCropName(String cropName);
 
-    default void updatePendingAndAcceptedOrderStatus(List<OrderItem> orderItems) {
-        List<OrderItem> pendingOrders = orderItems.stream()
-                .filter(orderItem -> orderItem.getOrderItemStatus() == OrderItem.OrderItemStatus.PENDING)
+    default void updatePendingAndAcceptedOrderStatus(List<Order> orders) {
+        List<Order> pendingOrders = orders.stream()
+                .filter(orderItem -> orderItem.getOrderStatus() == Order.OrderStatus.PENDING)
                 .toList();
 
-        List<OrderItem> acceptedOrders = orderItems.stream()
-                .filter(orderItem -> orderItem.getOrderItemStatus() == OrderItem.OrderItemStatus.ACCEPTED)
+        List<Order> acceptedOrders = orders.stream()
+                .filter(orderItem -> orderItem.getOrderStatus() == Order.OrderStatus.ACCEPTED)
                 .toList();
 
         pendingOrders.forEach(orderItem -> {
-            orderItem.setOrderItemStatus(OrderItem.OrderItemStatus.CANCELLED);
+            orderItem.setOrderStatus(Order.OrderStatus.CANCELLED);
             orderItem.setUpdatedAt(LocalDateTime.now());
         });
         acceptedOrders.forEach(orderItem -> {
-            orderItem.setOrderItemStatus(OrderItem.OrderItemStatus.CANCELLED);
+            orderItem.setOrderStatus(Order.OrderStatus.CANCELLED);
             orderItem.setUpdatedAt(LocalDateTime.now());
         });
     }
