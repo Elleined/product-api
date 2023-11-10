2d042de786f2cab75f438407123e03238bcd0712
Elleined <demadegu@gmail.com>
2023-11-10 21:29:19 +0800

    refactored regular buyer proxy

--- src/main/java/com/elleined/marketplaceapi/service/user/buyer/regular/RegularBuyerProxy.java
+++ src/main/java/com/elleined/marketplaceapi/service/user/buyer/regular/RegularBuyerProxy.java
@@ -1,6 +1,7 @@
 package com.elleined.marketplaceapi.service.user.buyer.regular;
 
-import com.elleined.marketplaceapi.dto.order.OrderDTO;
+import com.elleined.marketplaceapi.dto.order.RetailOrderDTO;
+import com.elleined.marketplaceapi.dto.order.WholeSaleOrderDTO;
 import com.elleined.marketplaceapi.exception.order.OrderAlreadyAcceptedException;
 import com.elleined.marketplaceapi.exception.order.OrderAlreadyRejectedException;
 import com.elleined.marketplaceapi.exception.order.OrderQuantiantyExceedsException;
@@ -11,8 +12,10 @@ import com.elleined.marketplaceapi.exception.resource.ResourceOwnedException;
 import com.elleined.marketplaceapi.exception.user.NotOwnedException;
 import com.elleined.marketplaceapi.exception.user.buyer.BuyerAlreadyRejectedException;
 import com.elleined.marketplaceapi.exception.user.buyer.BuyerMaxOrderPerDayException;
+import com.elleined.marketplaceapi.model.order.RetailOrder;
+import com.elleined.marketplaceapi.model.order.WholeSaleOrder;
 import com.elleined.marketplaceapi.model.user.User;
-import com.elleined.marketplaceapi.repository.order.OrderRepository;
+import com.elleined.marketplaceapi.service.order.OrderService;
 import com.elleined.marketplaceapi.service.user.buyer.BuyerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,6 +26,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.elleined.marketplaceapi.model.order.Order.Status.PENDING;
+
 @Service
 @Slf4j
 @Transactional
@@ -30,56 +35,46 @@ import java.util.List;
 public class RegularBuyerProxy implements BuyerService, RegularBuyerRestriction {
     private final BuyerService buyerService;
 
-    private final OrderRepository orderRepository;
-    public RegularBuyerProxy(@Qualifier("buyerServiceImpl") BuyerService buyerService,
-                             OrderRepository orderRepository) {
+    public RegularBuyerProxy(@Qualifier("buyerServiceImpl") BuyerService buyerService) {
         this.buyerService = buyerService;
-        this.orderRepository = orderRepository;
     }
 
-
     @Override
-    public OrderItem orderProduct(User buyer, OrderDTO orderDTO)
-            throws ResourceNotFoundException,
-            ResourceOwnedException,
-            ProductHasPendingOrderException,
-            ProductHasAcceptedOrderException,
-            ProductRejectedException,
-            ProductAlreadySoldException,
-            ProductNotListedException,
-            OrderQuantiantyExceedsException,
-            BuyerAlreadyRejectedException,
-            BuyerMaxOrderPerDayException,
-            ProductExpiredException {
-
+    public RetailOrder order(User buyer, RetailOrderDTO retailOrderDTO) throws ResourceNotFoundException, ResourceOwnedException, ProductHasPendingOrderException, ProductHasAcceptedOrderException, ProductRejectedException, ProductAlreadySoldException, ProductNotListedException, OrderQuantiantyExceedsException, BuyerAlreadyRejectedException, ProductExpiredException {
         if (isBuyerExceedsToMaxOrderPerDay(buyer))
             throw new BuyerMaxOrderPerDayException("Cannot order product! because you already reached the max order per day which is " + MAX_ORDER_PER_DAY + " consider buying premium account to remove this restriction.");
         // Add more validation for regular buyer here for future
-        return buyerService.orderProduct(buyer, orderDTO);
+        return buyerService.order(buyer, retailOrderDTO);
     }
 
     @Override
-    public List<OrderItem> getAllOrderedProductsByStatus(User currentUser, OrderItem.OrderItemStatus orderItemStatus) {
+    public WholeSaleOrder order(User buyer, WholeSaleOrderDTO wholeSaleOrderDTO) throws ResourceNotFoundException, ResourceOwnedException, ProductHasPendingOrderException, ProductHasAcceptedOrderException, ProductRejectedException, ProductAlreadySoldException, ProductNotListedException, OrderQuantiantyExceedsException, BuyerAlreadyRejectedException, ProductExpiredException {
+        if (isBuyerExceedsToMaxOrderPerDay(buyer))
+            throw new BuyerMaxOrderPerDayException("Cannot order product! because you already reached the max order per day which is " + MAX_ORDER_PER_DAY + " consider buying premium account to remove this restriction.");
         // Add more validation for regular buyer here for future
-        return buyerService.getAllOrderedProductsByStatus(currentUser, orderItemStatus);
+        return buyerService.order(buyer, wholeSaleOrderDTO);
     }
 
     @Override
-    public void cancelOrderItem(User buyer, OrderItem orderItem)
-            throws NotOwnedException, OrderAlreadyAcceptedException, OrderReachedCancellingTimeLimitException, OrderAlreadyRejectedException {
+    public void cancelOrder(User buyer, RetailOrder retailOrder) throws NotOwnedException, OrderAlreadyAcceptedException, OrderReachedCancellingTimeLimitException, OrderAlreadyRejectedException {
         // Add more validation for regular buyer here for future
-        buyerService.cancelOrderItem(buyer, orderItem);
+        buyerService.cancelOrder(buyer, retailOrder);
+    }
+
+    @Override
+    public void cancelOrder(User buyer, WholeSaleOrder wholeSaleOrder) throws NotOwnedException, OrderAlreadyAcceptedException, OrderReachedCancellingTimeLimitException, OrderAlreadyRejectedException {
+        buyerService.cancelOrder(buyer, wholeSaleOrder);
     }
 
     @Override
     public boolean isBuyerExceedsToMaxOrderPerDay(User buyer) {
-        final LocalDateTime currentDateTimeMidnight = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
-        final LocalDateTime tomorrowMidnight = currentDateTimeMidnight.plusDays(1);
-        return orderRepository.fetchBuyerOrderCount(
-                currentDateTimeMidnight,
-                tomorrowMidnight,
-                buyer,
-                OrderItem.OrderItemStatus.PENDING
-        ) >= MAX_ORDER_PER_DAY;
+        final LocalDateTime todayMidnight = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+        final LocalDateTime tomorrowMidnight = todayMidnight.plusDays(1);
+
+        List<RetailOrder> retailOrders = OrderService.getByDateRange(buyer.getRetailOrders(), PENDING, todayMidnight, tomorrowMidnight);
+        List<WholeSaleOrder> wholeSaleOrders = OrderService.getByDateRange(buyer.getWholeSaleOrders(), PENDING, todayMidnight, tomorrowMidnight);
+
+        int totalOrderToday = retailOrders.size() + wholeSaleOrders.size();
+        return totalOrderToday >= MAX_ORDER_PER_DAY;
     }
 }
