8aa00eb3dd35acf678fff572509483f62c6c7b3b
Elleined <demadegu@gmail.com>
2023-11-13 11:45:04 +0800

    tested save methodin cartitemserviceimpl unitl product has accepted order

--- src/test/java/com/elleined/marketplaceapi/service/cart/retail/RetailCartItemServiceImplTest.java
+++ src/test/java/com/elleined/marketplaceapi/service/cart/retail/RetailCartItemServiceImplTest.java
@@ -1,7 +1,13 @@
 package com.elleined.marketplaceapi.service.cart.retail;
 
+import com.elleined.marketplaceapi.dto.cart.RetailCartItemDTO;
+import com.elleined.marketplaceapi.exception.product.ProductExpiredException;
+import com.elleined.marketplaceapi.exception.product.ProductHasAcceptedOrderException;
+import com.elleined.marketplaceapi.exception.product.ProductHasPendingOrderException;
+import com.elleined.marketplaceapi.exception.resource.AlreadyExistException;
 import com.elleined.marketplaceapi.mapper.cart.RetailCartItemMapper;
 import com.elleined.marketplaceapi.model.cart.RetailCartItem;
+import com.elleined.marketplaceapi.model.order.RetailOrder;
 import com.elleined.marketplaceapi.model.product.Product;
 import com.elleined.marketplaceapi.model.product.RetailProduct;
 import com.elleined.marketplaceapi.model.user.User;
@@ -12,13 +18,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static com.elleined.marketplaceapi.model.order.Order.Status.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -81,17 +90,151 @@ class RetailCartItemServiceImplTest {
         List<RetailCartItem> expected = Arrays.asList(retailCartItem);
 
         assertEquals(expected.size(), actual.size());
-        for (int i = 0; i < actual.size() && i < expected.size(); i++) {
-            assertEquals(expected.get(i), actual.get(i));
-        }
+        assertIterableEquals(expected, actual);
     }
 
     @Test
     void delete() {
+        User user = User.builder()
+                .retailCartItems(new ArrayList<>())
+                .build();
+        RetailCartItem retailCartItem = new RetailCartItem();
+        user.getRetailCartItems().add(retailCartItem);
+
+        doAnswer(i -> user.getRetailCartItems().remove(retailCartItem))
+                .when(retailCartItemRepository)
+                .delete(retailCartItem);
+
+        retailCartItemService.delete(user, retailCartItem);
+        List<RetailCartItem> actual = user.getRetailCartItems();
+        List<RetailCartItem> expected = Collections.emptyList();
+
+        verify(retailCartItemRepository).delete(retailCartItem);
+        assertIterableEquals(expected, actual);
     }
 
     @Test
-    void testDelete() {
+    void shouldThrowProductExpiredException() {
+        User user = new User();
+        RetailCartItemDTO retailCartItemDTO = RetailCartItemDTO.retailCartItemDTOBuilder()
+                .productId(1)
+                .build();
+
+        RetailProduct expiredRetailProduct = RetailProduct.retailProductBuilder()
+                .id(1)
+                .expirationDate(LocalDate.now().minusDays(1))
+                .build();
+
+        when(retailProductService.getById(1)).thenReturn(expiredRetailProduct);
+
+        assertThrows(ProductExpiredException.class, () -> retailCartItemService.save(user, retailCartItemDTO));
+        verifyNoInteractions(retailCartItemMapper, retailCartItemRepository);
+    }
+
+    @Test
+    void shouldThrowProductAlreadyInCartException() {
+        User user = User.builder()
+                .retailCartItems(new ArrayList<>())
+                .build();
+
+        RetailCartItemDTO retailCartItemDTO = RetailCartItemDTO.retailCartItemDTOBuilder()
+                .productId(1)
+                .build();
+
+        RetailProduct retailProduct = mock(RetailProduct.class);
+        retailProduct.setId(1);
+
+        RetailCartItem retailCartItem = RetailCartItem.retailCartItemBuilder()
+                .retailProduct(retailProduct)
+                .build();
+        user.getRetailCartItems().add(retailCartItem);
+
+        when(retailProductService.getById(1)).thenReturn(retailProduct);
+        when(retailProduct.isExpired()).thenReturn(false);
+
+        assertThrows(AlreadyExistException.class, () -> retailCartItemService.save(user, retailCartItemDTO));
+        verifyNoInteractions(retailCartItemMapper, retailCartItemRepository);
+    }
+
+    @Test
+    void shouldThrowProductHasPendingOrderException() {
+        User user = spy(User.class);
+        user.setRetailCartItems(new ArrayList<>());
+
+        RetailProduct retailProduct = mock(RetailProduct.class);
+        retailProduct.setId(1);
+
+        RetailCartItemDTO retailCartItemDTO = RetailCartItemDTO.retailCartItemDTOBuilder()
+                .productId(1)
+                .build();
+
+        List<RetailOrder> retailOrders = Arrays.asList(
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(PENDING)
+                        .build(),
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(SOLD)
+                        .build(),
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(CANCELLED)
+                        .build(),
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(REJECTED)
+                        .build()
+        );
+        when(user.getRetailOrders()).thenReturn(retailOrders);
+
+        when(retailProductService.getById(1)).thenReturn(retailProduct);
+        when(retailProduct.isExpired()).thenReturn(false);
+        when(user.isProductAlreadyInCart(retailProduct)).thenReturn(false);
+
+        assertThrowsExactly(ProductHasPendingOrderException.class, () -> retailCartItemService.save(user, retailCartItemDTO));
+        verifyNoInteractions(retailCartItemMapper, retailCartItemRepository);
+    }
+
+    @Test
+    void shouldThrowProductHasAcceptedOrderException() {
+        User user = spy(User.class);
+        user.setRetailCartItems(new ArrayList<>());
+
+        RetailProduct retailProduct = mock(RetailProduct.class);
+        retailProduct.setId(1);
+
+        RetailCartItemDTO retailCartItemDTO = RetailCartItemDTO.retailCartItemDTOBuilder()
+                .productId(1)
+                .build();
+
+        List<RetailOrder> retailOrders = Arrays.asList(
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(ACCEPTED)
+                        .build(),
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(SOLD)
+                        .build(),
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(CANCELLED)
+                        .build(),
+                RetailOrder.retailOrderBuilder()
+                        .retailProduct(retailProduct)
+                        .status(REJECTED)
+                        .build()
+        );
+        when(user.getRetailOrders()).thenReturn(retailOrders);
+
+        when(retailProductService.getById(1)).thenReturn(retailProduct);
+        when(retailProduct.isExpired()).thenReturn(false);
+        when(user.isProductAlreadyInCart(retailProduct)).thenReturn(false);
+        when(user.hasOrder(retailProduct, PENDING)).thenReturn(false);
+
+        assertThrowsExactly(ProductHasAcceptedOrderException.class, () -> retailCartItemService.save(user, retailCartItemDTO));
+        verifyNoInteractions(retailCartItemMapper, retailCartItemRepository);
     }
 
     @Test
