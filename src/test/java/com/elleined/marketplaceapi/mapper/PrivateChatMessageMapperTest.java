719e363a33746995f4b5bc5c4c7763650b358c42
Elleined <demadegu@gmail.com>
2023-11-15 11:43:18 +0800

    tested to entity in privatechatmessagemapper

---
+++ src/test/java/com/elleined/marketplaceapi/mapper/PrivateChatMessageMapperTest.java
@@ -0,0 +1,71 @@
+package com.elleined.marketplaceapi.mapper;
+
+import com.elleined.marketplaceapi.dto.message.PrivateChatMessageDTO;
+import com.elleined.marketplaceapi.model.message.ChatMessage;
+import com.elleined.marketplaceapi.model.message.ChatRoom;
+import com.elleined.marketplaceapi.model.message.prv.PrivateChatMessage;
+import com.elleined.marketplaceapi.model.message.prv.PrivateChatRoom;
+import com.elleined.marketplaceapi.model.product.Product;
+import com.elleined.marketplaceapi.model.product.RetailProduct;
+import com.elleined.marketplaceapi.model.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static com.elleined.marketplaceapi.model.message.ChatMessage.Status.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PrivateChatMessageMapperTest {
+
+    @InjectMocks
+    private PrivateChatMessageMapper privateChatMessageMapper = Mappers.getMapper(PrivateChatMessageMapper.class);
+
+    @Test
+    void toEntity() {
+        Product retailProduct = RetailProduct.retailProductBuilder()
+                .id(1)
+                .build();
+
+        User currentUser = User.builder()
+                .id(1)
+                .build();
+        PrivateChatRoom privateChatRoom = PrivateChatRoom.privateChatRoomBuilder()
+                .id(1)
+                .isSenderAcceptedAgreement(ChatRoom.Status.NOT_ACCEPTED)
+                .isReceiverAcceptedAgreement(ChatRoom.Status.NOT_ACCEPTED)
+                .productToSettle(retailProduct)
+                .sender(currentUser)
+                .receiver(User.builder()
+                        .id(2)
+                        .build())
+                .privateChatMessages(new ArrayList<>())
+                .build();
+
+        PrivateChatMessage actual = privateChatMessageMapper.toEntity(privateChatRoom, currentUser, "PIcture", "Message");
+
+        assertEquals(0, actual.getId());
+
+        assertNotNull(actual.getMessage());
+        assertNotNull(actual.getPicture());
+
+        assertNotNull(actual.getSender());
+        assertEquals(currentUser, actual.getSender());
+
+        assertNotNull(actual.getCreatedAt());
+
+        assertEquals(ACTIVE, actual.getStatus());
+        assertNotNull(actual.getStatus());
+
+        assertEquals(privateChatRoom, actual.getPrivateChatRoom());
+        assertNotNull(actual.getPrivateChatRoom());
+    }
+
+    @Test
+    void toDTO() {
+    }
+}
