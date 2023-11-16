46dae241d3c01d0519ac2068033fb42538f60f0a
Elleined <demadegu@gmail.com>
2023-11-17 00:01:24 +0800

    added wholesaleproductcontroller

---
+++ src/main/java/com/elleined/marketplaceapi/controller/product/WholeSaleProductController.java
@@ -0,0 +1,43 @@
+package com.elleined.marketplaceapi.controller.product;
+
+import com.elleined.marketplaceapi.dto.product.WholeSaleProductDTO;
+import com.elleined.marketplaceapi.mapper.product.WholeSaleProductMapper;
+import com.elleined.marketplaceapi.model.product.WholeSaleProduct;
+import com.elleined.marketplaceapi.model.user.User;
+import com.elleined.marketplaceapi.service.product.wholesale.WholeSaleProductService;
+import com.elleined.marketplaceapi.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/whole-sale-products")
+public class WholeSaleProductController {
+    private final WholeSaleProductService wholeSaleProductService;
+    private final WholeSaleProductMapper wholeSaleProductMapper;
+
+    private final UserService userService;
+
+    @GetMapping("/{currentUserId}")
+    public List<WholeSaleProductDTO> getAllExcept(@PathVariable("currentUserId") int currentUserId) {
+        User currentUser = userService.getById(currentUserId);
+        return wholeSaleProductService.getAllExcept(currentUser).stream()
+                .map(wholeSaleProductMapper::toDTO)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public WholeSaleProductDTO getById(@PathVariable("id") int id) {
+        WholeSaleProduct wholeSaleProduct = wholeSaleProductService.getById(id);
+        return wholeSaleProductMapper.toDTO(wholeSaleProduct);
+    }
+
+    @GetMapping("/search-by-crop-name")
+    public List<WholeSaleProductDTO> searchProductByCropName(@RequestParam("cropName") String cropName) {
+        return wholeSaleProductService.searchProductByCropName(cropName).stream()
+                .map(wholeSaleProductMapper::toDTO)
+                .toList();
+    }
+}
