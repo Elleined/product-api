6b141f5878769663ea59e5598ef88b719456343b
Elleined <demadegu@gmail.com>
2023-11-23 22:23:03 +0800

    tested crop service

--- src/test/java/com/elleined/marketplaceapi/service/CropServiceTest.java
+++ src/test/java/com/elleined/marketplaceapi/service/CropServiceTest.java
@@ -1,74 +1,70 @@
 package com.elleined.marketplaceapi.service;
 
+import com.elleined.marketplaceapi.mapper.CropMapper;
+import com.elleined.marketplaceapi.model.Crop;
+import com.elleined.marketplaceapi.repository.CropRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CropServiceTest {
 
-    @Test
-    void getById() {
-        // Mock data
-
-        // Stubbing methods
-
-        // Calling the method
-
-        // Assertions
-
-        // Behavior verification
-    }
+    @Mock
+    private CropRepository cropRepository;
+    @Mock
+    private CropMapper cropMapper;
+    @InjectMocks
+    private CropService cropService;
 
     @Test
     void notExist() {
         // Mock data
 
         // Stubbing methods
+        when(cropRepository.findAll()).thenReturn(Collections.singletonList(getMockCrop()));
 
         // Calling the method
-
         // Assertions
+        assertTrue(cropService.notExist("Not existing crop name"));
 
         // Behavior verification
-    }
-
-    @Test
-    void getAll() {
-        // Mock data
-
-        // Stubbing methods
-
-        // Calling the method
-
-        // Assertions
-
-        // Behavior verification
+        verify(cropRepository).findAll();
     }
 
     @Test
     void getByName() {
         // Mock data
+        Crop expected = Crop.builder()
+                .name("Crop")
+                .build();
 
         // Stubbing methods
+        when(cropRepository.findAll()).thenReturn(Collections.singletonList(expected));
 
         // Calling the method
+        Crop actual = cropService.getByName("cRop");
 
         // Assertions
+        assertEquals(expected, actual);
 
         // Behavior verification
+        verify(cropRepository).findAll();
     }
 
-    @Test
-    void save() {
-        // Mock data
-
-        // Stubbing methods
-
-        // Calling the method
-
-        // Assertions
-
-        // Behavior verification
+    private Crop getMockCrop() {
+        return Crop.builder()
+                .name("Crop")
+                .build();
     }
 }
--- src/test/java/com/elleined/marketplaceapi/service/user/seller/SellerServiceImplTest.java
+++ src/test/java/com/elleined/marketplaceapi/service/user/seller/SellerServiceImplTest.java
@@ -3,8 +3,14 @@ package com.elleined.marketplaceapi.service.user.seller;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.elleined.marketplaceapi.dto.product.RetailProductDTO;
 import com.elleined.marketplaceapi.mapper.product.RetailProductMapper;
 import com.elleined.marketplaceapi.mapper.product.WholeSaleProductMapper;
+import com.elleined.marketplaceapi.mock.MultiPartFileDataFactory;
+import com.elleined.marketplaceapi.model.Crop;
+import com.elleined.marketplaceapi.model.product.RetailProduct;
+import com.elleined.marketplaceapi.model.unit.RetailUnit;
+import com.elleined.marketplaceapi.model.user.User;
 import com.elleined.marketplaceapi.repository.order.OrderRepository;
 import com.elleined.marketplaceapi.repository.order.RetailOrderRepository;
 import com.elleined.marketplaceapi.repository.order.WholeSaleOrderRepository;
@@ -22,6 +28,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @ExtendWith(MockitoExtension.class)
 class SellerServiceImplTest {
@@ -88,18 +97,39 @@ class SellerServiceImplTest {
     }
 
     @Test
-    void saveProduct() {
+    void saveProduct() throws IOException {
         // Expected and Actual Value
 
         // Mock Data
+        User user = spy(User.class);
+
+        RetailProductDTO retailProductDTO = RetailProductDTO.retailProductDTOBuilder()
+
+                .build();
 
         // Stubbing methods
+        doReturn(true).when(user).isNotVerified();
 
+        when(cropService.notExist(anyString())).thenReturn(false);
+        when(cropService.save(anyString())).thenReturn(new Crop());
+        when(cropService.getByName(anyString())).thenReturn(new Crop());
+        when(retailUnitService.getById(anyInt())).thenReturn(new RetailUnit());
+        when(retailProductMapper.toEntity(any(RetailProductDTO.class), any(User.class), any(Crop.class), any(RetailUnit.class), anyString())).thenReturn(new RetailProduct());
+        when(retailProductRepository.save(any(RetailProduct.class))).thenReturn(new RetailProduct());
+        doNothing().when(imageUploader).upload(anyString(), any(MultipartFile.class));
         // Calling the method
-
         // Assertions
+        assertDoesNotThrow(() -> sellerService.saveProduct(user, retailProductDTO, MultiPartFileDataFactory.notEmpty()));
+
 
         // Behavior Verifications
+        verify(cropService).notExist(anyString());
+        verify(cropService).save(anyString());
+        verify(cropService).getByName(anyString());
+        verify(retailUnitService).getById(anyInt());
+        verify(retailProductMapper).toEntity(any(RetailProductDTO.class), any(User.class), any(Crop.class), any(RetailUnit.class), anyString());
+        verify(retailProductRepository).save(any(RetailProduct.class));
+        verify(imageUploader).upload(anyString(), any(MultipartFile.class));
     }
 
     @Test
