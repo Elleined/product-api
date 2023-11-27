b559c5c03cba0aa14b36212421bc29512134d8a5
Elleined <demadegu@gmail.com>
2023-11-27 23:04:35 +0800

    added dependencies of premiumsellerproxy

---
+++ src/test/java/com/elleined/marketplaceapi/service/user/seller/premium/PremiumSellerProxyTest.java
@@ -0,0 +1,240 @@
+package com.elleined.marketplaceapi.service.user.seller.premium;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.elleined.marketplaceapi.service.atm.machine.validator.ATMValidator;
+import com.elleined.marketplaceapi.service.fee.FeeService;
+import com.elleined.marketplaceapi.service.user.seller.SellerService;
+import com.elleined.marketplaceapi.service.user.seller.fee.SellerFeeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PremiumSellerProxyTest {
+
+    @Mock
+    private SellerFeeService sellerFeeService;
+    @Mock
+    private SellerService sellerService;
+    @Mock
+    private FeeService feeService;
+    @Mock
+    private ATMValidator atmValidator;
+    @InjectMocks
+    private PremiumSellerProxy premiumSellerProxy;
+    
+
+    @Test
+    void saleProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleSaleProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void saveProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleSaveProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void updateProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleUpdateProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void deleteProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleDeleteProduct() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void acceptOrder() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleAcceptOrder() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void rejectOrder() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleRejectOrder() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void soldOrder() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+
+    @Test
+    void wholeSaleSoldOrder() {
+        // Expected values
+
+        // Mock Data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assestions
+
+        // Behavior verification
+    }
+}
