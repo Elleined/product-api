5d2523be70cabd2de9453ebb71b8d78e8bc1a5a4
Elleined <demadegu@gmail.com>
2023-12-01 19:24:17 +0800

    fix orderrepository

--- src/main/java/com/elleined/marketplaceapi/repository/order/OrderRepository.java
+++ src/main/java/com/elleined/marketplaceapi/repository/order/OrderRepository.java
@@ -3,6 +3,6 @@ package com.elleined.marketplaceapi.repository.order;
 import com.elleined.marketplaceapi.model.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 
 }
