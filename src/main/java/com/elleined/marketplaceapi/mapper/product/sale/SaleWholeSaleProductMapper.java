f17bb68df99c853e5848389edb0477b94c86e16e
Elleined <demadegu@gmail.com>
2023-12-02 10:30:17 +0800

    added sale products mappers test

--- src/main/java/com/elleined/marketplaceapi/mapper/product/sale/SaleWholeSaleProductMapper.java
+++ src/main/java/com/elleined/marketplaceapi/mapper/product/sale/SaleWholeSaleProductMapper.java
@@ -5,18 +5,19 @@ import com.elleined.marketplaceapi.model.product.WholeSaleProduct;
 import com.elleined.marketplaceapi.model.product.sale.SaleWholeSaleProduct;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface SaleWholeSaleProductMapper {
 
     @Mappings({
-            @org.mapstruct.Mapping(target = "id", ignore = true),
+            @Mapping(target = "id", ignore = true),
 
-            @org.mapstruct.Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())"),
-            @org.mapstruct.Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())"),
-            @org.mapstruct.Mapping(target = "salePrice", expression = "java(salePrice)"),
-            @org.mapstruct.Mapping(target = "wholeSaleProduct", expression = "java(wholeSaleProduct)")
+            @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "salePrice", expression = "java(salePrice)"),
+            @Mapping(target = "wholeSaleProduct", expression = "java(wholeSaleProduct)")
     })
     SaleWholeSaleProduct toEntity(SaleWholeSaleRequest saleWholeSaleRequest,
                                   @Context WholeSaleProduct wholeSaleProduct,
---
+++ src/test/java/com/elleined/marketplaceapi/mapper/product/sale/SaleRetailProductMapperTest.java
@@ -0,0 +1,24 @@
+package com.elleined.marketplaceapi.mapper.product.sale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SaleRetailProductMapperTest {
+
+    @Test
+    void toEntity() {
+        // Expected/ Actual values
+
+        // Mock data
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Assertions
+
+        // Behavior verification
+    }
+}
