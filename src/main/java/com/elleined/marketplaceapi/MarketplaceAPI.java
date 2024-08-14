9dd5f79c10cbc2a7a37880a10b131db4b523ac14
Elleined <demadegu@gmail.com>
2024-08-14 20:20:53 +0800

    Changes in application properties and pom.xml basically changes in configurations

--- src/main/java/com/elleined/marketplaceapi/MarketplaceAPI.java
+++ src/main/java/com/elleined/marketplaceapi/MarketplaceAPI.java
@@ -2,18 +2,21 @@ package com.elleined.marketplaceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableTransactionManagement
-@EnableFeignClients
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @EnableScheduling
+@EnableFeignClients
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class MarketplaceAPI {
 
 	public static void main(String[] args) {
